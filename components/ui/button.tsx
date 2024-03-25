2b520bdc69bb0c03d5936928726875e9add4fa54
Lucas Della Bella <dellabella.lucas@gmail.com>
2024-03-25 13:46:39 -0700

    Set up most of widget to call replicate for image segmentation

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,12 +20,14 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
         link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
+        custom: '',
       },
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
+        custom: '',
       },
     },
     defaultVariants: {
